7279270abeee58946e6fc84443596667784eca38
gdonega <gdonegadev@outlook.com>
2023-02-23 10:22:00 -0300

    ✨ some other dates

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # 📅 Working and testing dates from java 8
 
 ![alt text](./_readme_imgs/dates_types.png)
-Got this img [from this site](https://stackoverflow.com/questions/34119441/how-to-initialize-a-variable-of-date-type-in-java)
+Got this img [from this site](https://stackoverflow.com/questions/28730136/should-i-use-java-util-date-or-switch-to-java-time-localdate)
+
+
+[Difference between ZonedDateTime and OffsetDateTime](https://www.baeldung.com/java-zoneddatetime-offsetdatetime#differences)
--- src/main/java/io/github/gdonega/App.java
+++ src/main/java/io/github/gdonega/App.java
@@ -42,6 +42,38 @@ public class App
         Set<String> allZoneIds = ZoneId.getAvailableZoneIds();
 
 
+
+        // ---------------------------------------------------------------------------> OffsetTime
+        // Offset Time - With UTC timezone
+        OffsetTime offsetTimeUtcNow1 = OffsetTime.now(ZoneOffset.UTC);
+        OffsetTime offsetTimeUtcNow2 = OffsetTime.now(ZoneId.of("UTC"));
+
+        // Offset Time - With PC's timezone
+        OffsetTime offsetTimePcTimezoneNow = OffsetTime.now();
+        // Converet to UTC
+        OffsetTime offsetTimePcTimezoneNowInUTC = offsetTimePcTimezoneNow.withOffsetSameInstant(ZoneOffset.UTC);
+
+        // Offset Time - Format: Tminute:secound:milisecond+timezon
+        // Zoned Date Time - Format: 12:52:34+09:00
+        OffsetTime offsetTimeFromString = OffsetTime.parse("12:52:34+09:00");
+        // Convert to UTC
+        OffsetTime offsetTimeFromStringInUTC = offsetTimeFromString.withOffsetSameInstant(ZoneOffset.UTC);
+
+
+
+        // ---------------------------------------------------------------------------> OffsetDateTime
+        // Offset Date Time - With UTC timezone
+        OffsetDateTime offsetDateTimeUtcNow1 = OffsetDateTime.now(ZoneOffset.UTC);
+        OffsetDateTime offsetDateTimeUtcNow2 = OffsetDateTime.now(ZoneId.of("UTC"));
+
+        // Offset Date Time - With PC's timezone
+        OffsetDateTime offsetDateTimePcTimezoneNow = OffsetDateTime.now();
+        // Convert to UTC
+        OffsetDateTime offsetDateTimePcTimezoneNowInUTC = offsetDateTimePcTimezoneNow.withOffsetSameInstant(ZoneOffset.UTC);
+
+
+
+        // ---------------------------------------------------------------------------> ZonedDateTime
         // Zoned DateTime Now - With UTC timezone
         ZonedDateTime zonedDateTimeUtcNow1 = ZonedDateTime.now(ZoneOffset.UTC);
         ZonedDateTime zonedDateTimeUtcNow2 = ZonedDateTime.now(ZoneId.of("UTC"));
@@ -51,12 +83,20 @@ public class App
         // Convert to UTC
         ZonedDateTime zonedDateTimePcTimezoneNowInUtc = zonedDateTimePcTimezoneNow.withZoneSameInstant(ZoneOffset.UTC);
 
-
         // Zoned Date Time - Format: yyyy-MM-ddThour:minute:secound:milisecond+timezone
         // Zoned Date Time - Format: 2013-02-01T12:52:34+09:00
         ZonedDateTime zonedDateTimeFromString = ZonedDateTime.parse("2013-02-01T12:52:34+09:00");
         // Convert to UTC
         ZonedDateTime zonedDateTimeFromStringInUTC = zonedDateTimeFromString.withZoneSameInstant(ZoneOffset.UTC);
+
+
+        //----------------------------> IMPORTANT
+        //
+        //
+        // Difference between ZonedDateTime and OffsetDateTime: https://www.baeldung.com/java-zoneddatetime-offsetdatetime#differences
+        //
+        //
+        //----------------------------> IMPORTANT
 
 
         // ======================================================================================================================
